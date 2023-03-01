3842103bf8e69c5b4b54350ab207e0117517b016
jasurrahmon1994 <jasurrahmon24@gmail.com>
2023-03-01 18:00:09 +0500

    first try

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,9 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        return org.apache.commons.lang3.StringUtils.isNumeric(str) && !(NumberUtils.toDouble(str, -1) > 0);
     }
 }
